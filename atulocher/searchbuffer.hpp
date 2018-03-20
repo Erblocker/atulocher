29ec8cd1d19fdaa3e361ba08d1f147f32d2382ea
cgoxopx <cgoxopx@qq.com>
2018-03-20 10:12:39 +0000

    upload

--- atulocher/searchbuffer.hpp
+++ atulocher/searchbuffer.hpp
@@ -12,7 +12,12 @@
 namespace atulocher{
     class searchbuffer{
         public:
-            std::list<std::string> searchstack;
+            typedef enum{
+                CONTINUE,
+                FINISH,
+                STOP,
+                NEXT
+            }SearchStatus;
             struct element{
                 element     *       next;
                 char                name[128];
@@ -28,41 +33,93 @@ namespace atulocher{
                     depend.clear();
                 }
             };
-            virtual void searchStep(std::list<int> & buffer){
+            virtual bool searchStep(int last=-1,int deep=0){
+                if(deep==0)lastdeep=0;
                 std::map<int,int> actives;
-                int ibuf1,ibuf2;
+                bool stop=false,finish=false;
+                int ibuf1,ibuf2,S;
+                this->times--;
                 for(auto it1:targets){
+                    if(stop)            break;  //searching has been stoped
+                    if(this->times<0)   break;  //times has been run out
+                    
+                    S=0;
+                    actives.clear();    //reset
+                    
                     for(auto it2:datas){
-                        getActiveByPair(it2.first.c_str(),it1.c_str(),&ibuf1,&ibuf2);
+                        getActiveByPair(it2.first.c_str(),it1.c_str(),last,&ibuf1,&ibuf2);
                         actives[ibuf1]+=ibuf2;
+                        S+=ibuf2;
                     }
+                    if(S==0)continue;
+                    
+                    auto actlist=new std::pair<int,int> [actives.size()];
+                    auto actptrs=new std::pair<int,int>*[actives.size()];
+                    int activelen=actives.size();
+                    int i=0;
+                    for(auto it:actives){
+                        actlist[i].first=it.first;
+                        actlist[i].second=it.second;
+                        actptrs[i]=&actlist[i];
+                        ++i;
+                    }
+                    std::sort(actptrs,actptrs+activelen,[](std::pair<int,int>* p1,std::pair<int,int>* p2){
+                        return p1->second > p2->second;
+                    });
+                    actives.clear();    //data have been wrote into actlist
+                    
+                    for(i=0;i<activelen;i++){
+                        if(((double)(actptrs[i]->second)/(double)S)<maxP)break;
+                        SearchStatus status=callactivity(actptrs[i]->first);
+                        if(status==CONTINUE){
+                            if((deep-lastdeep)<maxemptydepth && deep<maxdeep){
+                                if(searchStep(actptrs[i]->first,deep+1)){
+                                    status=checkStatus(actptrs[i]->first);
+                                    if(status==STOP){
+                                        stop=true;
+                                        break;
+                                    }else
+                                    if(status==FINISH){
+                                        stop=true;
+                                        finish=true;
+                                        break;
+                                    }else
+                                    if(status==NEXT){
+                                        break;
+                                    }
+                                }
+                            }
+                        }else
+                        if(status==FINISH){
+                            stop=true;
+                            finish=true;
+                            break;
+                        }else
+                        if(status==STOP){
+                            stop=true;
+                            break;
+                        }
+                    }
+                    
+                    delete [] actlist;
+                    delete [] actptrs;
+                    return finish;
                 }
-                auto actlist=new std::pair<int,int> [actives.size()];
-                auto actptrs=new std::pair<int,int>*[actives.size()];
-                int activelen=actives.size();
-                int i=0;
-                for(auto it:actives){
-                    actlist[i].first=it.first;
-                    actlist[i].second=it.second;
-                    actptrs[i]=&actlist[i];
-                    ++i;
-                }
-                std::sort(actptrs,actptrs+activelen,[](std::pair<int,int>* p1,std::pair<int,int>* p2){
-                    return p1->second > p2->second;
-                });
-                for(i=0;i<activelen;i++){
-                    buffer.push_back(actptrs[i]->first);
-                }
-                delete [] actlist;
-                delete [] actptrs;
+            }
+            virtual SearchStatus checkStatus(int active){
                 
             }
         public:
-            virtual void getActiveByPair(const char * d,const char * t,int * active,int * times)=0;
-        private:
+            virtual void getActiveByPair(const char * d,const char * t,int last,int * active,int * times)=0;
+            virtual SearchStatus callactivity(int)=0;
+            int maxemptydepth,maxdeep;
+            double maxP;
+            int times;      //操作次数（行动力）
+        protected:
             mempool<element> pool;
             std::map<std::string,element*>  datas;
             std::set<std::string>           targets;
+            int lastdeep;
         public:
             searchbuffer(){
                 
